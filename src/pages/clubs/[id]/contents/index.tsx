f0f84f787c03d5360250d9e1cc57bea47b510761
j11s22k33 <j11s22k33@gmail.com>
2021-02-24 15:49:01 +0900

    소스 병합

--- src/pages/clubs/[id]/contents/index.tsx
+++ src/pages/clubs/[id]/contents/index.tsx
@@ -4,10 +4,13 @@ import Navigation from "@/utils/Navigation";
 import { useRouter } from "next/router";
 import { useCallback, useEffect, useState } from "react";
 
+type UI_MODE = "UI_DEFAULT_BROWSING" | "UI_CONTENTS_BROWSING" | "UI_SCROLL_BROWSING";
+
 const Contents = () => {
   const router = useRouter();
   const [contents, setContents] = useState([]);
-
+  const [uiMode, setUiMode] = useState<UI_MODE>("UI_DEFAULT_BROWSING")
+ 
   const fetchContents = useCallback(async () => {
     const data = await getContentList({
       CLUB_ID: router.query.id as string,
@@ -79,13 +82,32 @@ const Contents = () => {
   return (
     <div id="root">
       <div id="navi" className="container">
+
+      {/* <!-- contents-utill : start --> */}
         <div className="contents-utill">
           <div className="entry-route">
             <span className="home">홈</span>
             <span>헬로클럽</span>
             <span>콘텐츠</span>
           </div>
+          
+          {/* <!-- key-guide : start --> */
+          uiMode==="UI_CONTENTS_BROWSING" &&
+          <div className="key-guide">
+            <span className="align-type"><i className="green"></i>정렬 (최신순)</span>
+            <div className="fast-move">    
+                <button type="button" className="prev"></button>
+                <span>페이지 이동</span>
+                <button type="button" className="next"></button>
+            </div>
+          </div>
+          /* <!-- key-guide : end --> */}
+
         </div>
+        {/* <!-- contents-utill : end --> */}
+
+        {/* <!-- tab : start --> */
+        uiMode==="UI_DEFAULT_BROWSING" &&
         <nav className="tabs-wrap">
           <ul className="nav-tabs" id="tab-navi">
             {["전체", "예배영상", "교회행사", "찬송가"].map((tab) => (
@@ -95,7 +117,10 @@ const Contents = () => {
             ))}
           </ul>
         </nav>
-        <div className="contents-list">
+        /* <!-- tab : end --> */}
+
+        {/* <!-- contents-list : start --><!-- active 클래스로 리스트 스케일과 스크롤 포커스를 제어합니다. --> */
+        <div className={"contents-list " + (uiMode==="UI_SCROLL_BROWSING" && "active")}>
           {contents.length === 0 && (
             <div className="empty-contents">
               <p>
@@ -103,12 +128,29 @@ const Contents = () => {
               </p>
             </div>
           )}
+
+          {/* <!-- list : start --> */}
           <ul id="item-navi">
             {contents.map((content) => (
               <ContentItem key={content.ID} content={content} />
             ))}
           </ul>
+          {/* <!-- list : end --> */}
+
+          {/* <!-- scroll : start --> */
+          uiMode!=="UI_DEFAULT_BROWSING" &&
+          <div className="scroll">
+              <span className="focus"><i>1</i></span>
+              <span className=""><i>2</i></span>
+              <span className=""><i>3</i></span>
+              <span className=""><i>4</i></span>
+              <span className=""><i>5</i></span>
+          </div>
+          /* <!-- scroll : end --> */}
+
         </div>
+        /* <!-- contents-list : end --> */}
+
       </div>
     </div>
   );
