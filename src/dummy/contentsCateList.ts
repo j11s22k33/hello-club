8ec059227470429de9c665ca5f1d0d967ec5a1d5
j11s22k33 <j11s22k33@gmail.com>
2021-02-25 16:01:13 +0900

    1

--- src/dummy/contentsCateList.ts
+++ src/dummy/contentsCateList.ts
@@ -5,8 +5,8 @@ const data: Array<ContentCategory> = new Array(size)
 
 for (let x = 0; x < size; x++) {
   data[x] = {
-    CATE_ID: `cate_id_${x}`,
-    NAME: `${x} > 카테고리`
+    CATE_ID: `CATE_${x}`,
+    NAME: `CATE_${x}`
   }  
 }
 
