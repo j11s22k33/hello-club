59366654da998c74cb81e1e5623eb2fcc7280d3b
j11s22k33 <j11s22k33@gmail.com>
2021-02-25 21:44:51 +0900

    useStateCallbackWrapper 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,48 +1,15 @@
 import { AppProps } from "next/app";
 import "@/assets/css/style.css";
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useStateCallbackWrapper } from "@/utils/common"
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  /* 
-  UI 업데이트 START
-  updateUI(cb) -> setUpdateUINum() -> useEffect([updateUINum]) -> cb()
-  */
-  const __updateUIListener = useRef({
-    effect: [],
-    layoutEffect: []
-  })
-  const [__updateUINum, __setUpdateUINum] = useState(0)
-  
-  useEffect(()=>{
-    const listeners = __updateUIListener.current.effect
-    const length = listeners.length
-    __updateUIListener.current.effect = []
-
-    console.log('[updateUI useEffect] __updateUINum:%o, listeners.length:%o', __updateUINum, length)
-
-    for (let x=0; x<length; x++) {
-      listeners[x]()
-    }
-  }, [__updateUINum])
-  
-  useLayoutEffect(()=>{
-    const listeners = __updateUIListener.current.layoutEffect
-    const length = listeners.length
-    __updateUIListener.current.layoutEffect = []
-    
-    console.log('[updateUI useLayoutEffect] __updateUINum:%o, listeners.length:%o', __updateUINum, length)
-
-    for (let x=0; x<length; x++) {
-      listeners[x]()
-    }
-  }, [__updateUINum])
+  const [updUI, setUpdUI, updUICbList] = useStateCallbackWrapper(0)
   
   function updateUI(layoutEffectCb?:Function, effectCb?:Function) {
-    layoutEffectCb && __updateUIListener.current.layoutEffect.push(layoutEffectCb)
-    effectCb && __updateUIListener.current.effect.push(effectCb)
-    __setUpdateUINum(c => c+1)
+    layoutEffectCb && updUICbList.current.layoutEffect.push(layoutEffectCb)
+    effectCb && updUICbList.current.effect.push(effectCb)
+    setUpdUI(c => c+1)
   }
-  /* <!-- UI 업데이트 END */
 
   return (
     <>
--- src/utils/common.tsx
+++ src/utils/common.tsx
@@ -2,7 +2,48 @@ import PopupNoticeImage from "@/components/PopupNoticeImage";
 import PopupNoticeText from "@/components/PopupNoticeText";
 import PopupNoticeTextImage from "@/components/PopupNoticeTextImage";
 import Notice from "@/models/Notice";
-import React from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+
+/**
+ * state, setState, cb 반환
+ * @param initVal 
+ */
+const useStateCallbackWrapper = (initVal:any):
+[
+  any,
+  React.Dispatch<any>,
+  React.MutableRefObject<{
+    effect: any[];
+    layoutEffect: any[];}>
+] => {
+  const [state, setState] = useState(initVal)
+  const cbList = useRef({
+    effect: [],
+    layoutEffect: []
+  })
+
+  useLayoutEffect(() => {
+    const list = cbList.current.layoutEffect 
+    const len = list.length
+    cbList.current.layoutEffect = []
+
+    for (let x=0; x<len; x++) {
+      list[x](state)
+    }
+  }, [state])
+
+  useEffect(() => {
+    const list = cbList.current.effect 
+    const len = list.length
+    cbList.current.effect = []
+
+    for (let x=0; x<len; x++) {
+      list[x](state)
+    }
+  }, [state])
+  
+  return [state, setState, cbList]
+}
 
 function createNoticePopup(props: { notice: Notice; navigation; hide }) {
   switch (props?.notice?.type) {
@@ -15,4 +56,4 @@ function createNoticePopup(props: { notice: Notice; navigation; hide }) {
   }
 }
 
-export { createNoticePopup };
+export { createNoticePopup, useStateCallbackWrapper };
