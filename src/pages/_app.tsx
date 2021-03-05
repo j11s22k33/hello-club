78350cbff5e66aec30c166414f843a30a43cc1df
장성권 <j11s22k33@gmail.com>
2021-03-05 13:50:16 +0900

    env 로그

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from "next/app";
 import "@/assets/css/style.scss";
 import { useStateCallbackWrapper } from "@/utils/common"
+import env from "@/config/env";
+console.log(env)
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   const [tmp, uTmp] = useStateCallbackWrapper(0)
